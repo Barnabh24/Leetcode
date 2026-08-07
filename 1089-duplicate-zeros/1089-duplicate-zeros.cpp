97e8c30318997fd89a7f3f80f2bf7846b976502a
Barnabh24 <barnabhkashyap@gmail.com>
2026-08-07 23:13:04 +0530

    Time: 0 ms (100%), Space: 14.3 MB (18.03%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        vector<int>ans;
+        int n = arr.size();
+        for(int i=0; i<n; i++) {
+            if(arr[i] == 0) {
+                ans.push_back(0);
+                ans.push_back(0);
+            }else {
+                ans.push_back(arr[i]);
+            }
+        }
+        ans.resize(n);
+        arr=ans;
+    }
+};
