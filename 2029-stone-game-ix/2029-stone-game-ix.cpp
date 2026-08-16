3a2680d2ef6d7d09b82e84ed5a5fdd2b4fcf8ae5
Barnabh24 <barnabhkashyap@gmail.com>
2026-08-16 23:00:14 +0530

    Time: 0 ms (100%), Space: 131.4 MB (6.09%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int zeroes = 0;
+        int ones = 0;
+        int twos = 0;
+
+        for(int i=0; i<stones.size(); i++) {
+            int rem = stones[i]%3;
+            if(rem==0) {
+                zeroes++;
+            }else if(rem == 1) {
+                ones++;
+            }else {
+                twos++;
+            }
+        }
+
+        if(zeroes%2==0) return ones>0 && twos>0;
+        else return abs(ones-twos) > 2;
+    }
+};
