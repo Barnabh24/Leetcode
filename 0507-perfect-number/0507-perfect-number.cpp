f347ae1d1c5cf050885235ed6d1b30b3f660592f
Barnabh24 <barnabhkashyap@gmail.com>
2026-09-12 23:42:47 +0530

    Time: 1062 ms (34.74%), Space: 7.8 MB (21.3%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int total = 0;
+
+        for(int i=1; i<=num/2; i++) {
+            if(num%i == 0) {
+                total += i;
+            }
+        }
+        return num == total;
+    }
+};
